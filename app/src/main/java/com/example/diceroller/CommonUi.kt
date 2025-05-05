aa56b4d7052d4a9c5c9f47bf76ee0fc2cdd0180c
colin.fannon <colinfannon@gmail.com>
2025-05-05 10:54:14 -0400

    Implemented DiceSelectionButton ui

---
+++ app/src/main/java/com/example/diceroller/CommonUi.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+enum class DiceValue(val drawableResource: Int, val label: String) {
+    D2(R.drawable.d2, "d2"),
+    D4(R.drawable.d4, "d4"),
+    D6(R.drawable.d6,"d6"),
+    D8(R.drawable.d8, "d8"),
+    D10(R.drawable.d10, "d10"),
+    D12(R.drawable.d12, "d12"),
+    D20(R.drawable.d20, "d20"),
+    D100(R.drawable.d20, "d100")
+}
+
+@Composable
+fun DiceSelectionButton(diceValue: DiceValue, onClick: (DiceValue) -> Unit) {
+    IconButton(onClick = { onClick(diceValue)}) {
+        Icon(painter =
+        painterResource(diceValue.drawableResource),
+            contentDescription = diceValue.label,
+            modifier = Modifier
+                .size(50.dp)
+
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DiceSelectionPreview() {
+    DiceRollerTheme {
+        DiceSelectionButton(DiceValue.D20) {
+        }
+    }
+}
