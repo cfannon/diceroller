1a0a74df5a9d8288e0294ff64d7881a1a2c7a1ad
colin.fannon <colinfannon@gmail.com>
2025-08-06 15:01:16 -0400

    Updated HistoryScreen UI

--- app/src/main/java/com/example/diceroller/history/HistoryScreen.kt
+++ app/src/main/java/com/example/diceroller/history/HistoryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.diceroller.history
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,7 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -16,10 +19,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
+// TODO: 'Clear History' CTA
+// TODO: Modify/Finalize UI for History Screen
 @Composable
 fun HistoryScreenLayout(
     modifier: Modifier = Modifier,
@@ -46,13 +53,18 @@ fun HistoryScreenLayout(
             )
         }
 
-        // Displays list of rolls if any have been made
-        // Otherwise displays message
+        // Displays list of rolls if any have been made, otherwise displays message
         if (uiState.historyList.isEmpty()) {
-            Text(
-                "No rolls have been made yet.",
-                modifier = Modifier.padding(8.dp)
-            )
+            Row (
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    "No rolls have been made yet.",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
         } else {
             LazyColumn(
                 Modifier.fillMaxWidth()
@@ -60,18 +72,24 @@ fun HistoryScreenLayout(
                 items(uiState.historyList) { entry ->
                     HistoryItem(entry = entry)
                 }
-            }
 
-            // TODO: 'Clear History' CTA
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp),
-            ) {
-                Text(
-                    text = "Clear History",
-                    fontSize = 12.sp
-                )
+                // TODO: 'Clear History' CTA
+                item {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(4.dp),
+                    ) {
+                        Text(
+                            text = "Clear History",
+                            fontSize = 16.sp,
+                            fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = modifier
+                                .clickable { viewModel.onClearHistoryClick() }
+                        )
+                    }
+                }
             }
         }
     }
@@ -79,29 +97,59 @@ fun HistoryScreenLayout(
 
 @Composable
 fun HistoryItem(entry: HistoryEntry) {
-
-    Row(
-        Modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        shape = androidx.compose.foundation.shape.RoundedCornerShape(8.dp)
     ) {
-        Icon(
-            painter = painterResource(entry.dice.drawableResource),
-            contentDescription = entry.dice.label,
-            modifier = Modifier
-                .size(48.dp, 48.dp)
-                .padding(4.dp)
-        )
-        HistoryItemLabel(text = entry.rollResult)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(end = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            Icon(
+                painter = painterResource(entry.dice.drawableResource),
+                contentDescription = entry.dice.label,
+                modifier = Modifier
+                    .size(48.dp, 48.dp)
+                    .padding(4.dp)
+            )
+            HistoryItemLabel(
+                finalResult = entry.finalRollResult,
+                resultBreakdown = entry.rollResultBreakdown,
+            )
+        }
     }
 }
 
 @Composable
-fun HistoryItemLabel(text: String) {
-    Text(text = text)
+fun HistoryItemLabel(
+    finalResult: String,
+    resultBreakdown: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = finalResult,
+            modifier = modifier
+                .padding(end = 4.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = resultBreakdown
+        )
+    }
 }
 
-@Preview
+@Preview (showBackground = true)
 @Composable
 private fun HistoryItemPreview() {
     HistoryItem(entry = HistoryEntry())
