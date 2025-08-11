1e8f65b3c39240398f3672357a28937dbb851c17
colin.fannon <colinfannon@gmail.com>
2025-08-11 13:57:09 -0400

    Added Settings route

--- app/src/main/java/com/example/diceroller/AppDestinations.kt
+++ app/src/main/java/com/example/diceroller/AppDestinations.kt
@@ -3,4 +3,5 @@ package com.example.diceroller
 object AppDestinations {
     const val DICE_ROLLER_ROUTE = "dice_roller"
     const val HISTORY_ROUTE = "history"
+    const val SETTINGS_ROUTE = "settings"
 }
